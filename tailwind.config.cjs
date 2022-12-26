83eac0e4148690eb9fe512207b3a160435943d30
이시형 <tlgud585@gmail.com>
2022-12-27 05:03:05 +0900

    Revert "style: fonts 추가 및 tailwind.config 세팅"
    This reverts commit e20f7ced87c9caf86cbdbf78cc725141d25271f4.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,7 +17,22 @@ module.exports = {
 			"roboto-slab": ["Roboto Slab", "serif"],
 			"space-mono": ["Space Mono", "monospace"],
 		},
-		extend: {},
+		fontSize: {
+			h1: ["6.25rem", { lineHeight: "7.5rem", letterSpacing: "-0.3125rem" }],
+			h2: ["1.75rem", { lineHeight: "2.125rem" }],
+			h3: ["1rem", { lineHeight: "1.1875rem", letterSpacing: "0.9375rem" }],
+			h4: ["0.1875rem", { lineHeight: "1rem", letterSpacing: "0.3125rem" }],
+			"body-1": ["0.875rem", { lineHeight: "1.125rem" }],
+			"body-2": ["0.75rem", { lineHeight: "0.875rem" }],
+		},
+		extend: {
+			backgroundImage: {
+				"primary-gradient": "linear-gradient(315deg, #2E325A 0%, #0E112A 100%)",
+			},
+			boxShadow: {
+				"primary-shadow": "-50px -50px 100px #272C5A, 50px 50px 100px #121530",
+			},
+		},
 	},
 	plugins: [],
 };

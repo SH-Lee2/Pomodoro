e20f7ced87c9caf86cbdbf78cc725141d25271f4
이시형 <tlgud585@gmail.com>
2022-12-27 03:52:21 +0900

    style: fonts 추가 및 tailwind.config 수정 fonts 추가, colors 추가

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,21 @@
 module.exports = {
 	content: ["./src/**/*.{html,tsx}"],
 	theme: {
+		colors: {
+			salmon: "#F87070",
+			"baby-blue": "#70F3F8",
+			heliotrope: "#D881F8",
+			purple: "#D7E0FF",
+			"hawkes-blue": "#1E213F",
+			white: "#FFFFFF",
+			silver: "#EFF1FA",
+			"black-russian": "#161932",
+		},
+		fontFamily: {
+			"kumbh-sans": ["Kumbh Sans", "sans-serif"],
+			"roboto-slab": ["Roboto Slab", "serif"],
+			"space-mono": ["Space Mono", "monospace"],
+		},
 		extend: {},
 	},
 	plugins: [],

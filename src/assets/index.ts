2b080c33a01df762ad5ea3f9057a3f465f2a786a
이시형 <tlgud585@gmail.com>
2022-12-28 05:15:58 +0900

    featL icon 추가 체크 아이콘

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,13 @@ import arrowUpIcon from "./icon-arrow-up.svg";
 import closeIcon from "./icon-close.svg";
 import settingIcon from "./icon-settings.svg";
 import logoIcon from "./logo.svg";
+import checkIcon from "./check-icon.svg";
 
-export { arrowDownIcon, arrowUpIcon, closeIcon };
+export {
+	arrowDownIcon,
+	arrowUpIcon,
+	closeIcon,
+	settingIcon,
+	logoIcon,
+	checkIcon,
+};
